3a1f07b259e74a027bd16629530bfee843b7b7a7
Alex Silva <suporte3al@gmail.com>
2023-02-28 13:51:09 -0300

    feat: get description in google-ads

--- scraper-google-ads/back/src/controllers/descriptionController.js
+++ scraper-google-ads/back/src/controllers/descriptionController.js
@@ -5,9 +5,9 @@ const url = 'https://www.google.com.br/'
 let itemCountInList = 0
 
 const scraper = async (productName) => {
-  const browser = await pup.launch({headless: false})
+  const browser = await pup.launch({ headless: false })
   const page = await browser.newPage()
-  
+
   itemCountInList = 0 //zerando o contador a cada nova chamada
 
   await page.goto(url, { waitUntil: 'domcontentloaded' })
@@ -17,7 +17,7 @@ const scraper = async (productName) => {
     page.waitForNavigation(),
     page.click('.FPdoLc > center > input')
   ])
-  
+
 
   // tratativa de erro nos casos em que muda a posição do Shopping
   const isShopping = await page.$eval('.MUFPAc > .hdtb-mitem:nth-child(2) > a', link => link.innerText === 'Shopping')
@@ -28,8 +28,8 @@ const scraper = async (productName) => {
       page.waitForNavigation({ timeout: 0, waitUntil: 'domcontentloaded' }),
       page.click('.MUFPAc > .hdtb-mitem:nth-child(2) > a')
     ])
-    ) : (
-      await Promise.all([
+  ) : (
+    await Promise.all([
       console.log('click 3'),
       page.waitForNavigation({ timeout: 0, waitUntil: 'domcontentloaded' }),
       await page.click('.MUFPAc > .hdtb-mitem:nth-child(3) > a')
@@ -37,18 +37,25 @@ const scraper = async (productName) => {
 
   )
 
-  
+
   await Promise.all([
-    page.waitForNavigation({timeout: 0, waitUntil: 'domcontentloaded'}),
+    page.waitForNavigation({ timeout: 0, waitUntil: 'domcontentloaded' }),
     await page.click('.xCpuod')
   ])
-  
-  let desc = []
 
-  await page.$$eval('.xpDPYb > li > span', item => item.map(description => desc.push(description.innerText)))
-  return desc
+  await page.waitForSelector('.sh-ds__trunc-txt.translate-content')
+
+  let description = ''
+
+  try {
+    description = await page.$eval('.sh-ds__full-txt.translate-content', desc => desc.innerText)
+  } catch (error) {
+    description = await page.$eval('.sh-ds__trunc-txt.translate-content', desc => desc.innerText)
+  }
+
+  console.log('desc: ', description)
+  
+  return description
 
   //await page.close()
 }
-
-(async () => console.log(await scraper('mangueira de jardim')))()
