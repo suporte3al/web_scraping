80ce834e8d2b639a3ac842ad2477c7757af2fea9
Alex Silva <suporte3al@gmail.com>
2023-03-02 11:30:07 -0300

    feat: add spinning on load

--- scraper-google-ads/front/src/components/skeleton/index.jsx
+++ scraper-google-ads/front/src/components/skeleton/index.jsx
@@ -1,45 +1,11 @@
 import styles from './main.module.css';
+import { MoonLoader } from 'react-spinners'
 
-export const ComponentSkeleton = ({
-  urlLink = '#',
-  image = 'https://raw.githubusercontent.com/suporte3al/assets_AL/main/CAPACETE%20AL%202%202.png',
-  price = '0.00',
-  title = 'realize uma busca',
-  loja = 'Atacado do Lojista' }) => {
+export const ComponentSkeleton = () => {
   return (
     <>
       <div className={styles.skeletonContainer}>
-        <div className={styles.skeletonTitle}>
-          <h3>LOADING...</h3>
-        </div>
-
-        <ul>
-          {Array(3)
-            .fill()
-            .map((item, index) => (
-              <li key={index}>
-                <div key={urlLink} className={styles.cards}>
-                  <div className={styles.itemCard}>
-                    <img src={image} alt="imagem do produto" />
-                    <div className={styles.detals}>
-                      <div className={styles.detalsItem}>
-                        <h4>preço:</h4>
-                        <div className={styles.description}>R$ {price}</div>
-                      </div>
-                      <div className={styles.detalsItem}>
-                        <h4>titulo:</h4>
-                        <div className={styles.description}>{title}</div>
-                      </div>
-                      <div className={styles.detalsItem}>
-                        <h4>origem:</h4>
-                        <a href={urlLink} target='_blank'><div className={styles.description}>{loja}</div></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            ))}
-        </ul>
+        <MoonLoader className='spinning' color="#36d7b7" />
       </div>
     </>
   )
